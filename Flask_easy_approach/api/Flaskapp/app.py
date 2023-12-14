ee1f21d334a096b351ffbbc7fbe79b4ff538c254
Flashness123 <80914791+Flashness123@users.noreply.github.com>
2023-12-14 23:33:14 +0100

    Add files via upload

---
+++ Flask_easy_approach/api/Flaskapp/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, abort, request
+import models
+from model import forecast, forecast_one_day
+
+app = Flask(__name__)
+
+@app.route('/')
+def introduction():
+    return 'Home Path of the Prophet API'
+
+
+@app.route("/forecast-one-day", methods=["POST"])
+def get_forecast_one_day():
+   
+    predictions = forecast_one_day()
+
+    if not predictions:
+        abort(400, "Model not foundd.")
+
+    return jsonify({"forecast": predictions})
+
+
+@app.route("/forecast-n-days", methods=["POST"])
+def get_forecast_n_days():
+    if not request.json or not 'days' in request.json:
+        predictions = forecast()
+    else:
+        predictions = forecast(request.json['days'])
+
+    if not predictions:
+        abort(400, "Model not found.")
+
+    return jsonify({"forecast": predictions})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
---
+++ Flask_easy_approach/api/Flaskapp/model.py
@@ -0,0 +1,39 @@
+import os
+import json
+import datetime
+import pandas as pd
+from prophet.serialize import model_from_json
+
+def forecast(days = 7):
+    #model_file = "api/Flaskapp/models/prophet_serialized_model.json"
+    model_file = "./models/prophet_serialized_model.json"
+
+    if not os.path.exists(model_file):
+        print("Model file could not be found")
+        return False
+
+    with open(model_file, 'r') as fin:
+        model = model_from_json(json.load(fin))
+
+    #generate future dates
+
+    dates = pd.date_range(start=datetime.datetime.now().date(), end=datetime.datetime.now().date() + datetime.timedelta(days=days), freq='D')
+    future = pd.DataFrame({"ds": dates})
+    forecast = model.predict(future)
+    return forecast[['ds', 'yhat']].tail(days).to_dict("records")
+
+def forecast_one_day():
+    #model_file = "api/Flaskapp/models/prophet_serialized_model.json"
+    model_file = "./models/prophet_serialized_model.json"
+
+    if not os.path.exists(model_file):
+        print("Model file could not be foundd")
+        return False
+    
+    with open(model_file, 'r') as fin:
+        model = model_from_json(json.load(fin)) 
+
+    dates = pd.date_range(start=datetime.datetime.now().date(), end=datetime.datetime.now().date() + datetime.timedelta(days=1), freq='D')
+    future = pd.DataFrame({"ds": dates})  
+    forecast = model.predict(future)
+    return forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(1).to_dict("records")
